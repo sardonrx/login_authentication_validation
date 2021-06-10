292b8e620e1efe9501cd8c8ead90e0c0b570604f
Susan Adesoji <adesojisusan.adeola@gmail.com>
2021-06-10 14:45:04 +0100

    database table

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
 
-@app.route('/')
+app.config['SQLALCHEMY_DATABASE__URI']='sqlite:///database.db'
+@app.route('/' )
 def home():
     return render_template('home.html')
 
 @app.route('/login')
 def login():
-    return render_template('login')
+    return render_template('login.html')
 
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+
 
 
 if __name__ == "__main__":
