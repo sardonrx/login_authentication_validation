534c3d2edea50561ca5b35e7522b7d0451f61a49
Susan Adesoji <adesojisusan.adeola@gmail.com>
2021-06-10 17:59:16 +0100

    validating users

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
+from enum import unique
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from flask_wtf import wtforms
+from wtforms import StringFields, PasswordField, SubmitFields 
+from wtforms.validators import InputRequired, Lenght, ValidationError
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///database.db'
+app.config['SECRET_KEY'] = 'thisissusansecretkey'
 
-app.config['SQLALCHEMY_DATABASE__URI']='sqlite:///database.db'
 @app.route('/' )
 def home():
     return render_template('home.html')
@@ -18,6 +24,11 @@ def register():
     return render_template('register.html')
 
 
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key= True)
+    username = db.Column(db.String(20), unique= True, nullable=False)
+    password = db.Column(db.String(80), nullable= False)
 
 
 if __name__ == "__main__":
